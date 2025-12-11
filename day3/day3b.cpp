40aad3209bc135eafefd98dfafc4e6c3d9ae4343
Edmund Xin <mxinburritos@gmail.com>
2025-12-11 12:09:39 -0800

    Finish day3b

--- day3/day3b.cpp
+++ day3/day3b.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
+unordered_map<string, unordered_map<int, long long>> c;
+
+long long max_subset(const string &s, int n) {
+    if (c.find(s) != c.end() && c[s].find(n) != c[s].end()) {
+        return c[s][n];
+    }
+    if (n == 0) {
+        return 0;
+    }
+    if (s.size() <= n) {
+        return stoll(s);
+    }
+
+    long long selected = max_subset(s.substr(0, s.size() - 1), n);
+    long long not_selected =
+        stoll(to_string(max_subset(s.substr(0, s.size() - 1), n - 1)) +
+              s[s.size() - 1]);
+    long long ans = max(selected, not_selected);
+    c[s][n] = ans;
+    return max(selected, not_selected);
+}
+
 int main() {
     long long ans = 0;
 
@@ -9,13 +31,7 @@ int main() {
     std::fstream in_file("./day3_input.txt");
 
     while (std::getline(in_file, input)) {
-        int max_jolt = -1;
-        for (int i = 0; i < input.size(); i++) {
-            for (int j = i + 1; j < input.size(); j++) {
-                string cand = string(1, input[i]) + string(1, input[j]);
-                max_jolt = max(max_jolt, stoi(cand));
-            }
-        }
+        long long max_jolt = max_subset(input, 12);
         ans += max_jolt;
     }
 
